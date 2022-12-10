b044fa5f36e5bfb5fa697d46dc0361450b79cbb2
msl2002 <msl2002@uw.edu>
2022-12-09 17:27:08 -0800

    final

--- Analysis.R
+++ Analysis.R
@@ -35,15 +35,6 @@ yearsmin <- co2_data %>%
   filter(year == min(year), na.rm = TRUE) %>% 
   pull(year)
 
-# Copy the line below to make a slider bar 
-sliderInput("slider1", label = h3("Slider"), min = 0, 
-            max = 100, value = 50)
-),
-column(4,
-       
-       # Copy the line below to make a slider range 
-       sliderInput("slider2", label = h3("Slider Range"), min = 0, 
-                   max = 100, value = c(40, 60))
 
 
 
